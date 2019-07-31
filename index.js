243b0461389e29ad5526dfb1cf580a879321fe5e
Tobie Milford <tobie.milford@gmail.com>
2019-07-30 20:57:52 -0700

    Added items are checkable!  But why the extra line?

--- index.js
+++ index.js
@@ -5,14 +5,7 @@
 
 // App should allow:
 
-// DONE enter items they need to purchase by entering text and hitting "Return"  
-// or clicking "Add item" button 
-    
-    // My thought process:
-    // 1) use '.submit' method on button class which runs function that:
-    //  a) activates preventDefault for button
-    //  b) userInput = button input 
-    //  c) append properly-classed HTML containing userInput to js container
+
 
 
 // 
@@ -31,12 +24,22 @@
 // .click()
 // .keyPress (see example from lesson)
 
+
+
 $(function () {
+    
+    //DONE Goal #1: enter items they need to purchase by entering text and hitting "Return"  
+    // or clicking "Add item" button 
+    
+    // My thought process:
+    // 1) use '.submit' method on button class which runs function that:
+    //  a) activates preventDefault for button
+    //  b) userInput = button input 
+    //  c) append properly-classed HTML containing userInput to js container
     $('#js-shopping-list-form').submit(event => {
         event.preventDefault();
         let userInput = $(this).find('input[name="shopping-list-entry"]').val();
         console.log(userInput);
-
         const userInputListItem = `<li>
             <span class="shopping-item">${userInput}</span>
             <div class="shopping-item-controls">
@@ -53,21 +56,20 @@ $(function () {
     });
 
         
-    // check and uncheck items on the list by clicking the "Check" button
+    // Goal 2: check and uncheck items on the list by clicking the "Check" button
     
-    // Outline:
-    // 1) Upon .click of li element, .toggleClass .shopping-item__checked 
-    // 2) Use .closest to target text of item above button being clicked
-    // 3) What about buttons that didn't exist when jQuery was run?
-
-
-         $('.shopping-item-toggle').click(event => {
-            console.log('clicked!'); 
+        // Outline:
+        // 1) Upon .click of button element, .toggleClass .shopping-item__checked 
+        // 2) Use .closest to target text of item above button being clicked
+        // 3) What about buttons that didn't exist when jQuery was run?
+    
+            //    ex.  (ul).on(‘click’, ‘li’, function{ })
+          $('.shopping-list').on('click', 'li', function() {
             event.preventDefault();
-            
-            //Why isn't this working??
-            $(this).closest("li span").toggleClass("shopping-item__checked");
-        });
+            $(event.target).closest("li").toggleClass("shopping-item__checked");
+          });
+         
+         
         
     
 });
